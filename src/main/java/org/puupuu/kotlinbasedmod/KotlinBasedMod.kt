2f35327ba7e87467f97bd7e6aadf6ec5a2c3fda7
Sylvain Glaize <mokona@puupuu.org>
2016-06-27 22:39:39 +0200

    Rewrite the main class as Kotlin object, using Forgelin.

---
+++ src/main/java/org/puupuu/kotlinbasedmod/KotlinBasedMod.kt
@@ -0,0 +1,27 @@
+package org.puupuu.kotlinbasedmod
+
+import net.minecraftforge.fml.common.Mod
+import net.minecraftforge.fml.common.Mod.EventHandler
+import net.minecraftforge.fml.common.event.FMLInitializationEvent
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent
+import org.apache.logging.log4j.Logger
+
+@Mod(modid = KotlinBasedMod.MODID, version = KotlinBasedMod.VERSION,
+        modLanguageAdapter = "io.drakon.forge.kotlin.KotlinAdapter")
+object KotlinBasedMod {
+    const val MODID = "kotlinbasedmod"
+    const val VERSION = "0.1"
+
+    private var logger: Logger? = null
+
+    @EventHandler
+    fun preInit(event: FMLPreInitializationEvent) {
+        logger = event.modLog
+    }
+
+    @EventHandler
+    fun init(event: FMLInitializationEvent) {
+        val test = TestKotlin(logger!!)
+        test.go()
+    }
+}
