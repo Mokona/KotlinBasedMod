a1819ada3d8c066ade278caf5334acf0b840d73c
Sylvain Glaize <mokona@puupuu.org>
2016-06-28 23:40:24 +0200

    Add registering of item to GameRegistry

--- src/main/kotlin/org/puupuu/kotlinbasedmod/ModBuilder.kt
+++ src/main/kotlin/org/puupuu/kotlinbasedmod/ModBuilder.kt
@@ -1,6 +1,17 @@
 package org.puupuu.kotlinbasedmod
 
+import net.minecraft.item.Item
+import net.minecraftforge.fml.common.registry.GameRegistry
+
 class ModBuilder() {
     fun preInit() {
+        with(Item()) {
+            setMaxStackSize(1)
+            setRegistryName(KotlinBasedMod.MODID, "kotlinbaseditem")
+            unlocalizedName = registryName.toString()
+
+            GameRegistry.register(this)
+        }
+
     }
 }
