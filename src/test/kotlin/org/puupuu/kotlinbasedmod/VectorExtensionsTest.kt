75b05b64f5be3aa2e126de786aae983d7ef0fd8e
Sylvain Glaize <mokona@puupuu.org>
2016-07-20 23:00:00 +0200

    Add working implementation for plus and minus.

--- src/test/kotlin/org/puupuu/kotlinbasedmod/VectorExtensionsTest.kt
+++ src/test/kotlin/org/puupuu/kotlinbasedmod/VectorExtensionsTest.kt
@@ -12,10 +12,21 @@ class VectorExtensionsTest : WordSpec() {
 
                 a + b shouldBe Vec3i(4, 4, 4)
             }
+
+            "respond to sub operator" {
+                val a = Vec3i(1, 2, 3)
+                val b = Vec3i(3, 2, 1)
+
+                a - b shouldBe Vec3i(-2, 0, 2)
+            }
         }
     }
 }
 
-infix operator fun Vec3i.plus(vec3i: Vec3i): Vec3i {
-    throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+infix operator fun Vec3i.minus(other: Vec3i): Vec3i {
+    return Vec3i(this.x - other.x, this.y - other.y, this.z - other.z)
+}
+
+infix operator fun Vec3i.plus(other: Vec3i): Vec3i {
+    return Vec3i(this.x + other.x, this.y + other.y, this.z + other.z)
 }
